3ff96095621110a8038b7bda031ec4e99f328e01
Keith Fahlgren <abdelazer@gmail.com>
2012-10-17 11:09:41 -0700

    New mini project for #bib12

---
+++ README.md
@@ -0,0 +1,13 @@
+Oration
+=======
+
+Oration captures a conference presentation as static HTML.
+
+Developer Setup
+~~~~~~~~~~~~~~~
+
+1. `git clone ...`
+2. `cd oration`
+3. `virtualenv --python=python2.7 --prompt="(oration)" ve`
+4. `source ve/bin/activate`
+5. `python setup.py develop --always-unzip`
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='oration',
+      version=version,
+      description="Oration captures a conference presentation",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Keith Fahlgren',
+      author_email='keith@safaribooksonline.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          "twitter >= 1.9",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
